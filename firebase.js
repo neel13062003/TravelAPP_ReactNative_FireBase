4624bb85d7f80f87b0bc56eee47dd7c11015e937
NeelSoni <neelnsoni13@gmail.com>
2023-12-31 02:10:38 +0530

    ++

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoK0dn7Gsxo2TSNTmc8IkyMNcVX5pp95s",
-  authDomain: "virtual-sylph-390212.firebaseapp.com",
-  projectId: "virtual-sylph-390212",
-  storageBucket: "virtual-sylph-390212.appspot.com",
-  messagingSenderId: "860404961517",
-  appId: "1:860404961517:web:a19b10685bcebb4daafce4",
-  measurementId: "G-D7D0FBSDZB"
+  
 };
 
 const app = initializeApp(firebaseConfig);
